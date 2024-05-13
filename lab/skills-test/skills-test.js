5a7861c6cfe4c8677da4b91a7fba4f59748d4828
disha2424 <146101487+disha2424@users.noreply.github.com>
2024-05-13 08:42:24 -0400

    Add files via upload

---
+++ lab/skills-test/skills-test.js
@@ -0,0 +1,30 @@
+function add(a, b) {
+    return a + b;
+}
+
+
+function makeItGreen() {
+    document.querySelector('.circle').style.backgroundColor = 'green';
+}
+
+
+console.log(add(2, 7)); 
+
+
+
+
+const recipe = {
+    name: "Grilled Cheese",
+    ingredients: ["Bread ", "Cheese", "butter"],
+      servings: 1,
+     instructions: {
+        step1: "Butter the two pieces of bread",
+
+        step2: "Put a generous pile of the mixed cheeses on the side  of bread, then top it with the other piece of bread,",
+
+        step3: "cook on a pan till golden brown"
+    }
+};
+
+
+console.log(recipe);
